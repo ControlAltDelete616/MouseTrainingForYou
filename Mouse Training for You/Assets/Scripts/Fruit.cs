ebcfa8b85f0c7053ce713d91e055822bc5347b1a
ControlAltDelete616 <fabrizio.florio616@gmail.com>
2020-02-12 16:12:41 +0100

    Spawn & Fruit Script added

---
+++ Mouse Training for You/Assets/Scripts/Fruit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject slicedFruitPrefab;
+    public float explosionforce = 5;
+
+    public void CreateSlicedFruit()
+    {
+        GameObject inst = Instantiate(slicedFruitPrefab, transform.position, transform.rotation);
+
+        Rigidbody[] rbOnSliced = inst.transform.GetComponentsInChildren<Rigidbody>();
+
+        foreach (Rigidbody r in rbOnSliced)
+        {
+            r.transform.rotation = Random.rotation;
+            r.AddExplosionForce(Random.Range(500, 1000), transform.position, explosionforce);
+        }
+
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateSlicedFruit();
+        }
+    }
+}
---
+++ Mouse Training for You/Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject fruitToSpawnPrefab;
+    public Transform[] spawnPlaces;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
